c59d9eb257f784f1fe2325b6cfcbb3ae746835a6
Wes Sankey <me@westonsankey.com>
2025-02-23 11:14:24 -0500

    update cors

--- service-a/index.ts
+++ service-a/index.ts
@@ -28,7 +28,7 @@ const app = new Hono();
 app.use(
   "/*",
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://csrf-app-a.vercel.app"],
     allowMethods: ["POST"],
     credentials: true,
   })
