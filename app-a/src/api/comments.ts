25ed2cbd3af39d250435618beae7f533cecaf6f5
Wes Sankey <me@westonsankey.com>
2025-02-23 11:08:00 -0500

    update endpoint

--- app-a/src/api/comments.ts
+++ app-a/src/api/comments.ts
@@ -1,15 +1,20 @@
 import { Comment } from "../Comments";
 
 export const getComments = async () => {
-  const response = await fetch("http://localhost:8080/api/comments");
+  const response = await fetch(
+    "https://westonsankey-csrf-service-a.fly.dev/api/comments"
+  );
   return response.json();
 };
 
 export const addComment = async (comment: Comment): Promise<Comment> => {
-  const response = await fetch("http://localhost:8080/api/comments", {
-    method: "POST",
-    body: JSON.stringify(comment),
-  });
+  const response = await fetch(
+    "https://westonsankey-csrf-service-a.fly.dev/api/comments",
+    {
+      method: "POST",
+      body: JSON.stringify(comment),
+    }
+  );
 
   return response.json();
 };
