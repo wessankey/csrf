d4064ceb072acc76b6c9a4b7aa57ac8c88c3cf16
Wes Sankey <me@westonsankey.com>
2025-03-02 12:17:03 -0500

    add payment endpoint

--- service-b/index.ts
+++ service-b/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { setCookie } from "hono/cookie";
+import { getCookie, setCookie } from "hono/cookie";
 import { cors } from "hono/cors";
 
 const app = new Hono();
@@ -25,6 +25,23 @@ app.post("/api/login", async (c) => {
     secure: true,
   });
   return c.json({ message: "Login successful" }, 200);
+});
+
+app.get("/api/pay", async (c) => {
+  const sessionId = getCookie(c, "sessionId");
+  if (!sessionId) {
+    return c.json({ message: "Unauthorized" }, 401);
+  }
+
+  const amount = c.req.query("amount");
+  const recipient = c.req.query("recipient");
+
+  return c.json(
+    {
+      message: `Payment of ${amount} to ${recipient} successful`,
+    },
+    200
+  );
 });
 
 const port = 3001;
