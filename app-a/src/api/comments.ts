d14a7ddafcbf2032d39fa46e5b3446c18b9f0fcd
Wes Sankey <me@westonsankey.com>
2025-03-02 12:02:49 -0500

    hardcode urls

--- app-a/src/api/comments.ts
+++ app-a/src/api/comments.ts
@@ -2,14 +2,14 @@ import { Comment } from "../pages/Comments";
 
 export const getComments = async () => {
   const response = await fetch(
-    `https://westonsankey-csrf-service-b.fly.dev/api/comments`
+    `https://westonsankey-csrf-service-a.fly.dev/api/comments`
   );
   return response.json();
 };
 
 export const addComment = async (comment: Comment): Promise<Comment> => {
   const response = await fetch(
-    `https://westonsankey-csrf-service-b.fly.dev/api/comments`,
+    `https://westonsankey-csrf-service-a.fly.dev/api/comments`,
     {
       method: "POST",
       body: JSON.stringify(comment),
