b5c054095f78e53dc748449bb727e3eb7d9d0023
Wes Sankey <me@westonsankey.com>
2025-03-11 06:31:02 -0400

    update

--- social/app/src/main.tsx
+++ social/app/src/main.tsx
@@ -1,17 +1,10 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { CommentPage } from "./pages/Comments.tsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CommentPage />} />
-      </Routes>
-    </BrowserRouter>
-  </QueryClientProvider>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
